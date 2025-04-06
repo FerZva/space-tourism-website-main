2aa08257dccc7ad067d6b575d2ebf038b5903cae
FerZva <fernandzva2019@outlook.es>
2025-04-05 22:17:51 -0600

    Layout structured, pages and routes created, responsive background created

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import Header from "./components/Header";
 
 export const metadata: Metadata = {
   title: "Space Tourism Website",
@@ -13,7 +14,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="antialiased">{children}</body>
+      <body className="antialiased w-full h-full">
+        <div className="bg-[url('/background-home-mobile.jpg')] md:bg-[url('/background-home-tablet.jpg')] lg:bg-[url('/background-home-desktop.jpg')] bg-cover bg-no-repeat bg-center min-h-screen text-white">
+          <Header />
+          {children}
+        </div>
+      </body>
     </html>
   );
 }
